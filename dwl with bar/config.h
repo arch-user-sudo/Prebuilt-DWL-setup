68a9f1de6da88b781c318bdcffb46fe521e5ba20
arch-user-sudo <jtgthebeatplugsa@gmail.com>
2025-12-05 00:36:57 +0200

    Polished up a few things

--- dwl with bar/config.h
+++ dwl with bar/config.h
@@ -23,8 +23,8 @@ static const float rootcolor[]             = COLOR(0x000000ff);
 static const float fullscreen_bg[]         = {0.0f, 0.0f, 0.0f, 1.0f}; /* You can also use glsl colors */
 static uint32_t colors[][3]                = {
 	/*               fg          bg          border    */
-	[SchemeNorm] = { 0xffffffff, 0x161616B3, 0x333333B3 },
-	[SchemeSel]  = { 0xffffffff, 0x303030B3, 0x666666B3 },
+	[SchemeNorm] = { 0xffffffff, 0x111111B3, 0x333333B3 },
+	[SchemeSel]  = { 0xffffffff, 0x333333B3, 0x666666B3 },
 	[SchemeUrg]  = { 0,          0,          0x770000B3 },
 };
 
