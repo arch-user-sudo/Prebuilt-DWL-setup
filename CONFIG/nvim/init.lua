0da29e69396e59920003752a69eeb54512187a24
arch-user-sudo <jtgthebeatplugsa@gmail.com>
2025-12-05 14:48:37 +0200

    Added nvim config and vim keybindings to wofi

---
+++ CONFIG/nvim/init.lua
@@ -0,0 +1,9 @@
+
+vim.cmd('syntax on')
+-- Enable true color support
+vim.opt.termguicolors = false
+-- Show line numbers
+vim.opt.number = true
+vim.opt.relativenumber = false  
+vim.cmd('colorscheme vim')
+vim.opt.clipboard = "unnamedplus"
